f8f1db7e271d78a736227f515b126baa02b381e9
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2024-03-29 11:57:52 -0500

    adding soft estop to make sure we have valid position before continuing

--- vention_rail_hardware_interface/src/vention_rail_hardware_interface.cpp
+++ vention_rail_hardware_interface/src/vention_rail_hardware_interface.cpp
@@ -304,7 +304,8 @@ namespace vention_rail_hardware_interface
             std::string pos_str = sendHTTPMessage(create_get_position_command(), sockfd_);
             // if the returned string contains "error", don't try to parse
             std::string estop_status_str = sendHTTPMessage(create_estop_status_command(), sockfd_);
-            if (estop_status_str.find("true") != string::npos)
+            hard_estopped = (estop_status_str.find("true") != string::npos);
+            if (hard_estopped)
             {
                 RCLCPP_FATAL(rclcpp::get_logger("RailEHardwareInterface"),"Check E-Stop Status!");
             }
@@ -324,6 +325,12 @@ namespace vention_rail_hardware_interface
                 }
             }
 
+            // condition to come out of a soft_estop is that the difference between the current position and desired
+            // position is less than the soft_estop_tolerance, and we are not estopped
+            if (soft_estopped){
+                soft_estopped = (abs(curr_position_ - hw_commands_positions_[0]) > soft_estop_tolerance) || hard_estopped;
+            }
+
             // write new velocity based on current position, velocity, and goal
 
             // calculate vel command using pid controller 
@@ -334,7 +341,7 @@ namespace vention_rail_hardware_interface
             if (abs(velocity_cmd) < 0.005) velocity_cmd = 0.0; 
             
             // write desired velocity to rail if not estopped
-            if (estop_status_str.find("true") == string::npos)
+            if (!hard_estopped && !soft_estopped)
             {
                 string vel_cmd_str = create_velocity_command(velocity_cmd);
                 string response = sendHTTPMessage(vel_cmd_str, sockfd_);
@@ -349,7 +356,7 @@ namespace vention_rail_hardware_interface
 
 
             // if we received an error, stop the rail. It is probably in estop state. Also command the current position
-            if (estop_status_str.find("true") != string::npos)
+            if (hard_estopped)
             {
                 sendHTTPMessage(create_stop_all_motion_command(), sockfd_);
                 RCLCPP_FATAL(
@@ -358,6 +365,7 @@ namespace vention_rail_hardware_interface
                 // update all desired positions available to the current position
                 hw_commands_positions_[0] = hw_states_positions_[0];
                 position_cmd_ = hw_states_positions_[0];
+                soft_estopped = true;
             }
             else if (is_stopped())
             {
